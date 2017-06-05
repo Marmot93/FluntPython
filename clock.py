3bdd5f6c456b0e30b7f3e41b8448279ca91de1e5
Marmot93 <654332905@qq.com>
2017-06-05 21:22:12 +0800

    1

---
+++ clock.py
@@ -0,0 +1,31 @@
+import time
+
+
+def colck(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_str = ','.join(repr(arg) for arg in args)
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+
+    return clocked
+
+
+@colck
+def snooze(seconds):
+    time.sleep(seconds)
+
+
+@colck
+def factorial(n):
+    return 1 if n < 2 else n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    print('*' * 40, 'Calling snooze(.123)')
+    snooze(.123)
+    print('*' * 40, 'Calling factorial(6)')
+    print('factorial(6)=', factorial(6))
