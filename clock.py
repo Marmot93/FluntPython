facc3b3fcd4f77e350371c3720cb336c67192452
Marmot93 <654332905@qq.com>
2017-06-05 22:14:46 +0800

    装饰器

--- clock.py
+++ clock.py
@@ -24,8 +24,36 @@ def factorial(n):
     return 1 if n < 2 else n * factorial(n - 1)
 
 
-if __name__ == '__main__':
-    print('*' * 40, 'Calling snooze(.123)')
-    snooze(.123)
-    print('*' * 40, 'Calling factorial(6)')
-    print('factorial(6)=', factorial(6))
+# if __name__ == '__main__':
+#     print('*' * 40, 'Calling snooze(.123)')
+#     snooze(.123)
+#     print('*' * 40, 'Calling factorial(6)')
+#     print('factorial(6)=', factorial(6))
+
+
+# 测试标准库装饰器,优化递归的@functools.lru_cache()
+# 1
+@colck
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n - 2) + fib(n - 1)
+
+
+if __name__ == "__main__":
+    print(fib(8))
+
+# 2
+import functools
+
+@functools.lru_cache()
+@colck
+def fib2(n):
+    if n < 2:
+        return n
+    else:
+        return fib2(n - 2) + fib2(n - 1)
+
+if __name__ == "__main__":
+    print(fib2(8))
