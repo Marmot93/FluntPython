b63bfff839504569985d8d03792d09cd1a0af82c
marmot <654332905@qq.com>
2017-09-28 11:01:02 +0800

    2017-9-28

---
+++ Module_of_the_Week/Data Structures/enum_learn.py
@@ -0,0 +1,36 @@
+import enum
+
+@enum.unique
+class BugStatus(enum.IntEnum):
+# class BugStatus(enum.Enum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+    # This will trigger an error with unique applied.
+    by_design = 4
+    closed = 1
+
+
+# actual_state = BugStatus.wont_fix
+# desired_state = BugStatus.fix_released
+#
+# print('Equality:',
+#       actual_state == desired_state,
+#       actual_state == BugStatus.wont_fix)
+# print('Identity:',
+#       actual_state is desired_state,
+#       actual_state is BugStatus.wont_fix)
+# print('Ordered by value:')
+# try:
+#     print('\n'.join('  ' + s.name for s in sorted(BugStatus)))
+# except TypeError as err:
+#     print('  Cannot sort: {}'.format(err))
+
+print('Ordered by value:')
+print('\n'.join('  ' + s.name for s in sorted(BugStatus)))
